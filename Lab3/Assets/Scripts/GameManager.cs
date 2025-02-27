1dd22e91b1c97ae9aa955599b9dd3b3cb1bd3c64
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-02-27 16:41:17 -0600

    Winscreen!

--- Lab3/Assets/Scripts/GameManager.cs
+++ Lab3/Assets/Scripts/GameManager.cs
@@ -7,19 +7,27 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI carrotCount;
     int collected;
     public int carrots;
+    public MusicPlayer jb;
+    bool winstate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnAwake() {
-        
+
     }
     void Start()
     {
+        jb = GameObject.FindWithTag("Respawn").GetComponent<MusicPlayer>();
         collected = 0;
         carrots = 0;
+        winstate = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (collected == 1 && !winstate) {
+            winstate = true;
+            Win();
+        }
         
     }
 
@@ -36,5 +44,10 @@ public class GameManager : MonoBehaviour
     public void DecrementCarrot() {
         carrots--;
         carrotCount.text = "Carrot: " + carrots;
+    }
+
+    void Win() {
+        Initiate.Fade("Congratulations", Color.black, 1.5f);
+        jb.GetComponent<AudioSource>().Stop();
     }
 }
